8fe53a187a2333a745f67b1c963c545a4d03a8d9
wei.gao <wave.gao2013@gmail.com>
2023-06-27 16:48:57 +1200

    add schema to separate file

--- src/containers/PasswordReset/index.tsx
+++ src/containers/PasswordReset/index.tsx
@@ -1,40 +1,14 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 
 import TextField from 'components/TextField';
 
 import './PasswordReset.scss';
+import schema from './passwordFormYupSchema';
 
 interface IFormInput {
   password: string;
 }
-
-const MIN_LENGTH = 8;
-
-const schema = yup
-  .object()
-  .shape({
-    password: yup
-      .string()
-      .required('Must not be empty')
-      .test('min_8_characters', 'Must contain at least 8 characters', (currentPassword) => {
-        if (currentPassword.length < MIN_LENGTH) {
-          return false;
-        }
-        return true;
-      })
-      .test('match_constrain', 'Must contain one number and two special characters', (currentPassword) => {
-        let isValid = true;
-        if (currentPassword.length >= 8 && currentPassword.length < 15) {
-          const regex = /^(?=.*\d)(?=.*[\W_].*[\W_])[A-Za-z0-9\W_]{8,}$/;
-          isValid = regex.test(currentPassword);
-        }
-
-        return isValid;
-      }),
-  })
-  .required();
 
 const PasswordReset = (): JSX.Element => {
   const initalState = { password: '' };
